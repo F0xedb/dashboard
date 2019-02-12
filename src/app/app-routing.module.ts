eefbbe961fa99b33b56f0e5ac7c84e6206513b5d
Tom Meyers <zeus@zeus.ap.be>
2019-02-12 12:00:50 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ListComponent} from './list/list.component';
-import {AddFormComponent} from './add-form/add-form.component';
+import { ListComponent } from './list/list.component';
+import { AddFormComponent } from './add-form/add-form.component';
 
 const routes: Routes = [
   { path: '', component: ListComponent },
-  { path: 'add', component: AddFormComponent}
+  { path: 'add', component: AddFormComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true, onSameUrlNavigation: 'reload'})],
+  imports: [RouterModule.forRoot(routes, { useHash: true, onSameUrlNavigation: 'reload' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
